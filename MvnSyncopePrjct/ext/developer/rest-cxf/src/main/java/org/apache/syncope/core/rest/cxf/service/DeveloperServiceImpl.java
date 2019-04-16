8dfa20fa371c1f8311a5f6a1437f8d62e58ec1d7
FeanorAmarth <43479612+FeanorAmarth@users.noreply.github.com>
2019-04-16 02:04:14 +0200

    Feanor
    remove swagger-ui

--- MvnSyncopePrjct/ext/developer/rest-cxf/src/main/java/org/apache/syncope/core/rest/cxf/service/DeveloperServiceImpl.java
+++ MvnSyncopePrjct/ext/developer/rest-cxf/src/main/java/org/apache/syncope/core/rest/cxf/service/DeveloperServiceImpl.java
@@ -27,17 +27,20 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
 @Service
-@RestController
+//@Controller
+//@RequestMapping("developer/")
 public class DeveloperServiceImpl extends AbstractServiceImpl implements DeveloperService {
 
     @Autowired
     private DeveloperLogic devlogic;
 
     @Override
-	@GetMapping("/developer")
+//	@GetMapping("developer")
     public DeveloperLogic developer() {
         return devlogic.developer();
     }
